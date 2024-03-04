742da621fe7f4db9007b662ee7b6a5e2b596de4d
daniel <tsumimagal@gmail.com>
2024-03-04 16:00:48 +0200

    added to the testing and need to fix db_dan

--- db_dan.py
+++ db_dan.py
@@ -1,15 +1,21 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 import nacl.secret
 import nacl.utils
+from sqlalchemy.testing.pickleable import Address, User
+
+
 def main():
-
-    engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-    from sqlalchemy import text
-
-    with engine.connect() as conn:
-        result = conn.execute(text("select 'hello world'"))
-        print(result.all())
+    engine = create_engine("sqlite://", echo=True)
+    with Session(engine) as session:
+        exmpl = User(
+            name = "user",
+            fullname = "User U",
+            addresses = [Address(email_address="user@sqlalchemy.org")]
+        )
+        session.add(exmpl)
+        session.commit()
 if __name__ == "__main__":
     main()
--- testing.py
+++ testing.py
@@ -1,24 +1,24 @@
-import hashlib
-
-
-
-def file_hash_generator(file):
-    '''
-
-    :param file:
-    :return: the hash of a file
-    '''
-    with open(file, 'rb') as f:
-        m = hashlib.sha1()
-        m.update(f.read())
-    return m.hexdigest()
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.testing.pickleable import User, Address
 
 
 def main():
-    ec = file_hash_generator(r'C:\Users\cyber\Desktop\testing.txt')
-    print(ec)
+    engine = create_engine("sqlite://", echo = True)
+    with Session(engine) as session:
+        spongebob = User(
+            name="spongebob",
+            fullname="Spongebob Squarepants",
+            addresses=[Address(email_address="spongebob@sqlalchemy.org")],
+        )
+        # patrick = User(name="patrick", fullname="Patrick Star")
+        session.add(spongebob)
+        stm = select(User).where(User.name == "spongebob")
+        spongebob = session.scalar(stm).one()
+        spongebob.addresses.appened(Address(email_address = "spongebob@gmail.com"))
+        session.commit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main___":
     main()
